53d93958b485c9f30ee14cd46882f58147ca5688
imbatcat <ptdat04@gmail.com>
2025-06-13 15:28:48 +0700

    #6 fix notification provder bug

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Navigator from "./navigation/Navigator";
+import { NotificationProvider } from "./context/notificationContext";
 
 export default function App() {
   return (
